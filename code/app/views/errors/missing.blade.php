2a19160a957cd399345a822c05818da610739c56
Ferrrrrre <ferrelambert@hotmail.com>
2014-11-12 14:49:27 +0000

    Final commit

---
+++ code/app/views/errors/missing.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+@section('title', '404 Error')
+@section('container')
+
+<h1>Oei, deze pagina kennen we nog niet?</h1>
+<h3>Als u denkt dat we deze wel zouden moeten kennen, neem dan zeker <a href="mailto:ferre.lambert@student.kdg?subject=404">contact</a> met ons op!</h3>
+
+<p><a href="/">Keer terug naar de homepage</a></p>
+
+@stop
